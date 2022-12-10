510e6a1b2c8875a98591d8ea747394a05fc723ba
rk254 <rodatar@gmail.com>
2022-12-10 12:32:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 /**
- * function
- * with
- * void 
- * return
- */
-
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
