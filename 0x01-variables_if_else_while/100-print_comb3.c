1e102ce4cdbcbd8910dbd751582e263c158e5e2c
rk254 <rodatar@gmail.com>
2022-12-13 19:13:17 -0800

    ;100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- Entry - point
+ *
+ * Return: Always 0 (SUccess)
+ */
+
+int main(void)
+{
+	int d1, d2;
+
+	for (d1 = '0'; d1< '9'; ++d1)
+	{
+		for (d2 = d1 + 1; d2 <= '9'; ++d2)
+		{
+			if (d2 != d1)
+			{
+				putchar(d1);
+				putchar(d2);
+
+				if (d1 == '8' && d2 == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
